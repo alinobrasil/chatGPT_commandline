321b55472cce0b8c82182132e2c9f28701a6771c
Ali Kim <alikim@gmail.com>
2022-12-13 11:54:14 -0500

    initial commit

---
+++ chat.py
@@ -0,0 +1,30 @@
+import openai
+import config
+
+# set the API key
+openai.api_key = config.openai_key
+
+# set the model to use (e.g. "text-davinci-002")
+model = "text-davinci-003"
+
+prompt = ""
+
+while prompt != "quit()":
+        
+
+    # set the prompt
+    prompt = input("\nAsk me anything. Type quit() to terminate: ")
+
+    # generate text
+    completions = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # print the generated text
+    generated_text = completions.choices[0].text
+    print(generated_text)
---
+++ readme.md
@@ -0,0 +1,7 @@
+To run this, you need python installed. Just go with the latest version.
+
+first, run this in your terminal:
+```pip install openai```
+
+then just run this:
+```python3 chat.py```

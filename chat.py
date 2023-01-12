ca83c7dac6d1c54bd3b4c0af98eec75558c2dd4d
Ali Kim <alikim@gmail.com>
2023-01-12 13:00:03 -0300

    ditch config, just let user input api key

--- chat.py
+++ chat.py
@@ -1,8 +1,10 @@
 import openai
 import config
 
+mykey = input("\n\n****enter your API key: ")
+
 # set the API key
-openai.api_key = config.openai_key
+openai.api_key = mykey
 
 # set the model to use (e.g. "text-davinci-002")
 model = "text-davinci-003"
@@ -13,18 +15,24 @@ while prompt != "quit()":
         
 
     # set the prompt
-    prompt = input("\nAsk me anything. Type quit() to terminate: ")
+    prompt = input("\n\n****Ask me anything. Type quit() to terminate:       ")
+    try:
+        if prompt != "":
+            # generate text
+            completions = openai.Completion.create(
+                engine=model,
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
 
-    # generate text
-    completions = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-
-    # print the generated text
-    generated_text = completions.choices[0].text
-    print(generated_text)
+            # print the generated text
+            generated_text = completions.choices[0].text
+            print(generated_text)
+    except openai.error.AuthenticationError:
+        print("api error")
+        mykey = input("\n\n****enter your API key: ")
+        openai.api_key = mykey
+    # endif

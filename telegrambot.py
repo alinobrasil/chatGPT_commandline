dc0b1bd0dbb780d6edb831b602e286cb8a62e7b0
Ali Kim <alikim@gmail.com>
2023-03-24 23:50:50 -0400

    updated to work with gpt3.5

--- telegrambot.py
+++ telegrambot.py
@@ -9,7 +9,9 @@ openai.api_key = config.openai_key
 # openai_api_key = config.openai_key
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm your GPT3 bot. Please enter your message to get a response from OpenAI's GPT3 engine.")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="Hi! I'm your GPT3 bot. Ask me anything!")
 
 def respond(update, context):
     message = update.message.text
@@ -17,15 +19,18 @@ def respond(update, context):
     print("\nPrompt-----------------------")
     print(name)
     print(message)
-    completions = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=message,
+    completions = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": message},
+        ],
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.7,
     )
-    generated_text = completions.choices[0].text
+    # print(completions)
+    generated_text = completions.choices[0]["message"]["content"]
     print("\nResponse---------------------")
     print(generated_text)
     context.bot.send_message(chat_id=update.effective_chat.id, text=generated_text)
